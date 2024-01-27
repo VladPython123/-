7a05e8407d42c101cd86114fde2287b857c2b974
VladPython123 <156468245+VladPython123@users.noreply.github.com>
2024-01-27 18:14:56 +0200

    Create admin_keyboard.py

---
+++ keyboard/admin_keyboard.py
@@ -0,0 +1,18 @@
+from aiogram import types 
+
+buttons01 = [
+           [types.InlineKeyboardButton(text="Пітвердети", callback_data="Пітвердети"),],
+           [types.InlineKeyboardButton(text="Відмінити", callback_data="Відмінити"), ] ,
+           ]
+
+buttons2 = [
+           [types.InlineKeyboardButton(text="Аналітика", callback_data="Аналітика"),],
+           [types.InlineKeyboardButton(text="Змінити товари", callback_data="Змінити товари"), ] ,
+           ]
+
+buttons3 = [
+           [types.InlineKeyboardButton(text="⬅️Меню", callback_data="Меню2"),],
+           ]
+
+
+
